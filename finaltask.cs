3cb88f1b774e3acf7cb3ac2841fa37095f2b9ee6
[Gaia <[dzhayadzoy@gmail.com]>
2023-09-14 14:53:13 +0500

    added create new array method

--- finaltask.cs
+++ finaltask.cs
@@ -1,3 +1,15 @@
 ﻿string[] array = new string [6] {"lotus", "+", "kitty", "square", "recepie", "fun"};
 string[] array1 = new string [array.Length];
 
+void NewArr (string [] array, string [] array1) {
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i].Length <= 3)
+    array1[count] = array[i];
+    count ++; 
+}
+}
+
+
