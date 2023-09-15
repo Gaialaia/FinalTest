b22087585a04bf75c0ec924db3ad3eae27d11deb
[Gaia <[dzhayadzoy@gmail.com]>
2023-09-15 12:39:59 +0500

    removed comments

--- finaltask.cs
+++ finaltask.cs
@@ -29,22 +29,7 @@ void PrintArr (string[] array) {
 NewArr(array,array1);
 PrintArr(array1);
 
-// string[] M;
-// Console.WriteLine("Введите массив строк через запятую");
-// string stroka = Console.ReadLine();
-// M = stroka.Split(',');
-// var result = new string[M .Length];
-// var realSize = 0;
-// foreach (var value in M )
-// {
-//     if (value.Length <= 3)
-//     {
-//         result[realSize] = value;
-//         realSize++;
-//     }
-// }
-// Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
-// Console.ReadKey(true);
+
 
 
 
